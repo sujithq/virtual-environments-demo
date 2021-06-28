6d14eb3011cfec196aad62792389ed4735e3cb60
Sujith Quintelier <sujith.quintelier@gmail.com>
2021-06-28 08:47:50 +0200

    Add Random Stuff

--- tf/main.tf
+++ tf/main.tf
@@ -4,10 +4,20 @@ terraform {
       source="hashicorp/azurerm"
       version="=2.65.0"
     }
+    random = {
+      source="hashicorp/random"
+      version="=3.1.0"
+    }
   }
 }
 
 provider "azurerm" {
   features {
   }
+}
+
+resource "random_pet" "sg" {}
+
+output "web-address" {
+  value = random_pet.sg.id
 }
